b078d9e6081670bbefbce0634d90236cb6d1db61
Bolu Akinsola <boluakins@gmail.com>
2021-05-28 23:16:51 +0100

    created controller

---
+++ 3Lineng/CardDetails.API/Commons/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CardDetails.API.Commons
+{
+    public static class Utilities
+    {
+        public static string GetHash(string input)
+        {
+            var result = Array.Empty<byte>();
+            using (var sha512Hash = new SHA512Managed())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                result = sha512Hash.ComputeHash(inputBytes);
+            }
+            return Convert.ToBase64String(result);
+        }
+    }
+}
---
+++ 3Lineng/CardDetails.API/Controllers/CardSchemeController.cs
@@ -0,0 +1,35 @@
+﻿using CardDetails.API.DTOs;
+using CardDetails.API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CardDetails.API.Controllers
+{
+    [ApiController]
+    [Route("card-scheme")]
+    [Authorize]
+    public class CardSchemeController : ControllerBase
+    {
+        private readonly ICardSchemeService cardSchemeService;
+
+        public CardSchemeController(IServiceProvider service)
+        {
+            cardSchemeService = service.GetRequiredService<ICardSchemeService>();
+        }
+        [HttpGet("verify/{cardNumber}")]
+        public IActionResult Verify([FromRoute] string cardNumber)
+        {
+            var result = cardSchemeService.Verify(cardNumber);
+            return Ok(result);
+        }
+
+        [HttpGet("stats")]
+        public IActionResult Stats([FromQuery] Metadata metadata)
+        {
+            var result = cardSchemeService.Stats(metadata);
+            return Ok(result);
+        }
+    }
+}
