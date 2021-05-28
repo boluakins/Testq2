60ccd1d2c480b7c5dfc5f234eb3b668534f05fde
Bolu Akinsola <boluakins@gmail.com>
2021-05-28 23:27:41 +0100

    added service

---
+++ 3Lineng/CardDetails.API/Services/CardSchemeService.cs
@@ -0,0 +1,42 @@
+﻿using CardDetails.API.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace CardDetails.API.Services
+{
+    public class CardSchemeService : ICardSchemeService
+    {
+        public VerifyEndpointResponse Verify(string cardNumber)
+        {
+            var fetchedFromImaginaryRepo = new VerifyEndpointResponse
+            {
+                Success = true,
+                Payload = new VerifyResponseEndpointPayload
+                {
+                    Scheme = "visa",
+                    Type = " debit",
+                    Bank = "UBS"
+                }
+            };
+            return fetchedFromImaginaryRepo;
+        }
+        public StatsEndpointResponse Stats(Metadata metadata)
+        {
+            var fetchedFromImaginaryRepo = new StatsEndpointResponse
+            {
+                Success = true,
+                Start = metadata.Start,
+                Limit = metadata.Limit,
+                Size = 133,
+                Payload = new Dictionary<string, int>
+                {
+                    { "545423", 5},
+                    { "679234", 4},
+                    { "329802", 1}
+                }
+            };
+            return fetchedFromImaginaryRepo;
+        }
+    }
+}
---
+++ 3Lineng/CardDetails.API/Services/ICardSchemeService.cs
@@ -0,0 +1,11 @@
+﻿
+using CardDetails.API.DTOs;
+
+namespace CardDetails.API.Services
+{
+    public interface ICardSchemeService
+    {
+        VerifyEndpointResponse Verify(string cardNumber);
+        StatsEndpointResponse Stats(Metadata metadata);
+    }
+}
