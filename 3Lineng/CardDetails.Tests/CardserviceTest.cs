aa7d954a38314e4e8f68ae91bad9ea856457f80b
Bolu Akinsola <boluakins@gmail.com>
2021-05-29 08:31:55 +0100

    added test

---
+++ 3Lineng/CardDetails.Tests/CardserviceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CardDetails.Tests
+{
+    public class CardserviceTest
+    {
+        public void ShouldReturnRequestedDataSuccessfully()
+        {
+            //Arrange
+
+
+            // Act
+
+
+            // Assert
+        }
+    }
+}
